0639a9594c2fc3f9ffe465d406f5465255715625
wuchuheng <root@wuchuheng.com>
2023-01-29 20:49:43 +0800

    feat: add the color output feature

--- main.go
+++ main.go
@@ -5,12 +5,23 @@ import (
 	"crypto/rand"
 	"crypto/tls"
 	"fmt"
+	"github.com/fatih/color"
 	"log"
 	"net"
+	"sync"
 	"time"
 )
 
+type ClientInfo struct {
+	clientId  int
+	mutex     *sync.Mutex
+	clientSet *map[int]bool
+}
+
 func main() {
+	var clientId = 0
+	clientList := make(map[int]bool)
+	mutex := sync.Mutex{}
 	cert, err := tls.LoadX509KeyPair(
 		"certs/client.pem",
 		"certs/private.key",
@@ -27,15 +38,20 @@ func main() {
 	}
 	for {
 		conn, err := listener.Accept()
+		clientId++
+		mutex.Lock()
+		clientList[clientId] = true
+		mutex.Unlock()
+		clientInfo := ClientInfo{clientId: clientId, mutex: &mutex, clientSet: &clientList}
 		if err != nil {
 			log.Println(err)
 			continue
 		}
-		go handleConn(conn)
+		go handleConn(conn, clientInfo)
 	}
 }
 
-func handleConn(conn net.Conn) {
+func handleConn(conn net.Conn, clientInfo ClientInfo) {
 	defer conn.Close()
 
 	cert, err := tls.LoadX509KeyPair(
@@ -47,21 +63,34 @@ func handleConn(conn net.Conn) {
 	}
 	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
 	imapConn, err := tls.Dial("tcp", "imap.qq.com:993", &config)
-	//imapConn, err := tls.Dial("tcp", "imap.163.com:993", &config)
 	if err != nil {
 		log.Fatalf("client: dial: %s", err)
 	}
-	go handleImapConn(imapConn, conn)
+	go handleImapConn(imapConn, conn, clientInfo)
 
 	r := bufio.NewReader(conn)
+
 	for {
 		msg, err := r.ReadString('\n')
 		if err != nil {
+			clientInfo.mutex.Lock()
+			delete(*clientInfo.clientSet, clientInfo.clientId)
+			clientInfo.mutex.Unlock()
 			log.Println(err)
 			return
 		}
 		t := time.Now()
-		println("-> " + t.Format("2006-01-02 15:04:05") + " " + msg)
+
+		fmt.Println(
+			convertColorStrByClientId(clientInfo.clientId,
+				fmt.Sprintf(
+					"C%d: %s  ",
+					clientInfo.clientId, msg[:len(msg)-2],
+				),
+			),
+			fmt.Sprintf("online: %s dateTime: %s", clientSetToStr(clientInfo), t.Format("2006-01-02 15:04:05")),
+		)
+
 		n, err := imapConn.Write([]byte(msg))
 		if err != nil {
 			log.Println(n, err)
@@ -70,7 +99,7 @@ func handleConn(conn net.Conn) {
 	}
 }
 
-func handleImapConn(imapConn net.Conn, parenConn net.Conn) {
+func handleImapConn(imapConn net.Conn, parenConn net.Conn, info ClientInfo) {
 	r := bufio.NewReader(imapConn)
 	for {
 		msg, err := r.ReadString('\n')
@@ -79,12 +108,46 @@ func handleImapConn(imapConn net.Conn, parenConn net.Conn) {
 			return
 		}
 		t := time.Now()
-		println("<- " + t.Format("2006-01-02 15:04:05") + " " + msg)
 
+		colorStr := convertColorStrByClientId(info.clientId, fmt.Sprintf("S%d: %s ",
+			info.clientId,
+			msg[:len(msg)-2],
+		))
+		logStr := fmt.Sprintf("%s online: %s dateTime: %s \n", colorStr, clientSetToStr(info), t.Format("2006-01-02 15:04:05"))
+		fmt.Println(logStr)
 		n, err := parenConn.Write([]byte(msg))
 		if err != nil {
 			log.Println(n, err)
 			return
 		}
 	}
+}
+
+func clientSetToStr(info ClientInfo) string {
+	result := ""
+	info.mutex.Lock()
+	for k, _ := range *info.clientSet {
+		result = fmt.Sprintf("%s C%d", result, k)
+	}
+	info.mutex.Unlock()
+	return fmt.Sprintf("[%s]", result[1:])
+}
+
+type IdMapColor map[int]func(str string) string
+
+func convertColorStrByClientId(clientId int, content string) string {
+	idMapColor := IdMapColor{
+		0: func(str string) string {
+			return color.BlueString(str)
+		},
+		1: func(str string) string {
+			return color.RedString(str)
+		},
+		2: func(str string) string {
+			return color.GreenString(str)
+		},
+	}
+	colorId := clientId % len(idMapColor)
+
+	return idMapColor[colorId](content)
 }
